469ea29362de83c1b939f826f39df84a031bb8dd
Arthur <arthur.wilcke@gmail.com>
2017-12-03 19:23:50 +0000

    new models

--- LearnToLearn/models.py
+++ LearnToLearn/models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
+import copy
 
 class LayerNorm(nn.Module):
 
@@ -27,15 +28,15 @@ class SVMRegressor(nn.Module):
         self.main = nn.Sequential(
             nn.Linear(4097, 6144),
             nn.BatchNorm1d(6144),
-            *d,
+            copy.deepcopy(*d),
             nn.LeakyReLU(negative_slope=slope, inplace=True),
             nn.Linear(6144, 5120),
             nn.BatchNorm1d(5120),
-            *d,
+            copy.deepcopy(*d),
             nn.LeakyReLU(negative_slope=slope, inplace=True),
             nn.Linear(5120, 4097),
             nn.BatchNorm1d(4097),
-            *d,
+            copy.deepcopy(*d),
             nn.LeakyReLU(negative_slope=slope, inplace=True),
             nn.Linear(4097, 4097)
             )
@@ -72,7 +73,105 @@ class SVMRegressor(nn.Module):
 
         return l2_loss, hinge_loss
 
-class Critic(nn.Module):
+class Generator8(SVMRegressor):
+    def __init__(self, dropout=False, slope=0.01, square_hinge=False, n_gpu=1):
+        super().__init__(dropout=dropout, slope=slope, square_hinge=square_hinge, n_gpu=n_gpu)
+
+        # optional dropout param
+        d = [nn.Dropout()] if dropout else []
+        self.main = nn.Sequential(
+            nn.Linear(4097, 4097),
+            nn.BatchNorm1d(4097),
+            copy.deepcopy(*d),
+            nn.LeakyReLU(negative_slope=slope, inplace=True),
+            nn.Linear(4097, 6144),
+            nn.BatchNorm1d(6144),
+            copy.deepcopy(*d),
+            nn.LeakyReLU(negative_slope=slope, inplace=True),
+            nn.Linear(6144, 6144),
+            nn.BatchNorm1d(6144),
+            copy.deepcopy(*d),
+            nn.LeakyReLU(negative_slope=slope, inplace=True),
+            nn.Linear(6144, 5120),
+            nn.BatchNorm1d(5120),
+            copy.deepcopy(*d),
+            nn.LeakyReLU(negative_slope=slope, inplace=True),
+            nn.Linear(5120, 5120),
+            nn.BatchNorm1d(5120),
+            copy.deepcopy(*d),
+            nn.LeakyReLU(negative_slope=slope, inplace=True),
+            nn.Linear(5120, 4097),
+            nn.BatchNorm1d(4097),
+            copy.deepcopy(*d),
+            nn.LeakyReLU(negative_slope=slope, inplace=True),
+            nn.Linear(4097, 4097),
+            nn.BatchNorm1d(4097),
+            copy.deepcopy(*d),
+            nn.LeakyReLU(negative_slope=slope, inplace=True),
+            nn.Linear(4097, 4097)
+            )
+
+class UGen(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.fc1 = self.make_fc(4097, 4096)
+        self.fc2 = self.make_fc(4096, 2048)
+        self.fc3 = self.make_fc(2048, 1024)
+        self.fc4 = self.make_fc(1024, 2048)
+        self.fc5 = self.make_fc(4096, 4096)
+        self.fc6 = self.make_fc(8192, 4096)
+        self.fc7 = self.make_fc(8192, 4097)
+        self.fc8 = self.make_fc(8194, 4097)
+
+    def make_fc(self, in_dim, out_dim):
+        return nn.Sequential(
+                nn.Linear(in_dim, out_dim),
+                nn.BatchNorm1d(out_dim),
+                nn.Dropout(),
+                nn.LeakyReLU(negative_slope=0.01, inplace=True)
+                )
+
+    def forward(self, x):
+
+        # normal layers
+        out_1 = self.fc1(x)
+        out_2 = self.fc2(out_1)
+        out_3 = self.fc3(out_2)
+        out_4 = self.fc4(out_3)
+        # skip connections
+        out_5 = self.fc5(torch.cat(out_4, out_2))
+        out_6 = self.fc6(torch.cat(out_5, out_1))
+        out_7 = self.fc7(torch.cat(out_6, x))
+        return self.fc8(out_7)
+
+    def loss(self, regressed_w, w1, train):
+        l2_loss = .5 * torch.mean(torch.norm(w1.sub(regressed_w), 2, 1).pow(2))
+
+        hinge_loss = Variable(torch.zeros(1))
+        if torch.cuda.is_available():
+            hinge_loss = hinge_loss.cuda()
+
+        # calculate summed hinge loss of each regressed svm
+        for i in range(regressed_w.data.shape[0]):
+            hinge_vector = torch.clamp(
+                    1 - torch.matmul(
+                        regressed_w[i][:-1], train[i].transpose(0,1))
+                    .add(regressed_w[i][-1]),
+                    min=0)
+
+            # square the hinge loss if requested
+            hinge_vector = torch.clamp(hinge_vector, max=100).pow(2) if self.square_hinge else hinge_vector
+
+            hinge_loss += torch.mean(hinge_vector)
+        
+        # average
+        hinge_loss.div_(regressed_w.data.shape[0])
+
+        return l2_loss, hinge_loss
+
+
+class Critic3(nn.Module):
 
     def __init__(self, n_gpu=1, gp=True):
         super().__init__()
@@ -100,7 +199,7 @@ class Critic(nn.Module):
             output = self.main(x)
         return output
 
-class LargeCritic(nn.Module):
+class Critic4(nn.Module):
 
     def __init__(self, n_gpu=1, gp=True):
         super().__init__()
