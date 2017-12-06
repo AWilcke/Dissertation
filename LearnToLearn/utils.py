f588fb5367b973ee5f378bb62031c7b46b59145b
Arthur Wilcke <s1452854@tatties.inf.ed.ac.uk>
2017-12-06 13:45:21 +0000

    didnt like new axis

--- LearnToLearn/utils.py
+++ LearnToLearn/utils.py
@@ -8,7 +8,6 @@ def make_graph_image(x, y):
     fig = plt.figure()
     plt.plot(x,y)
     plt.xticks(x)
-    plt.axis([x[0], x[-1], 0, 1])
     fig.canvas.draw()
 
     w, h  = fig.canvas.get_width_height()
